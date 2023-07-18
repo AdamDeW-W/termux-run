11e2302e74af11376ae9099954d1a964dc2fe187
Adam DeW <lowkeycoat1@gmail.com>
2023-07-17 21:50:04 -0500

    added 'which' and 'man' install

--- unpack.sh
+++ unpack.sh
@@ -4,4 +4,14 @@ pkg update -y
 
 pkg upgrade -y
 
+if (which which)
+then
+echo "which already installed"
+else pkg install which
+fi
 
+if (which man)
+then
+echo "man already installed"
+else pkg install man
+fi
