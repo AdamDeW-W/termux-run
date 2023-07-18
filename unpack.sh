bf2223682f961123fff3a5a75c93e94bae0ae135
Adam DeW <lowkeycoat1@gmail.com>
2023-07-17 21:56:20 -0500

    added comments on 'which' and 'man'

--- unpack.sh
+++ unpack.sh
@@ -4,14 +4,20 @@ pkg update -y
 
 pkg upgrade -y
 
+#checking if which is installed
 if (which which)
 then
 echo "which already installed"
+
+#installing which
 else pkg install which
 fi
 
+#checking if 'man' is installed
 if (which man)
 then
 echo "man already installed"
+
+#installing 'man'
 else pkg install man
 fi
